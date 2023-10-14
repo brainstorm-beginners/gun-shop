92f58fe8583f59efdb0f7342e46681b297a79408
Shell <nyancat2452@gmail.com>
2023-10-15 00:03:58 +0600

    Add multiply filter

--- api/repositories/gun_repository.py
+++ api/repositories/gun_repository.py
@@ -1,7 +1,9 @@
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Query
+from sqlalchemy import or_
 
 from models.models import Gun
 from models.schemas import GunCreate
@@ -47,14 +49,53 @@ class GunRepository:
             raise HTTPException(status_code=404, detail="Guns not found")
         return guns_by_barrel_type
 
-    async def get_guns_by_name(self, name: str):
+    async def get_guns_by_name(self, names: str):
         try:
-            result = await self.session.execute(select(Gun))
-            guns = result.scalars().all()
-            guns_by_name = list()
-            for gun in guns:
-                if name in gun.name:
-                    guns_by_name.append(gun)
+            query = select(Gun).where(or_(*[Gun.name.ilike(f"%{name}%") for name in names]))
+            result = await self.session.execute(query)
+            guns_by_name = result.scalars().all()
         except NoResultFound:
             raise HTTPException(status_code=404, detail="Guns not found")
         return guns_by_name
+
+    async def filter_guns(self, filters: dict):
+        try:
+            query = Query(Gun)
+
+            filter_list = []
+
+            for key, value in filters.items():
+                if key == 'name':
+                    clean_values = [value.strip().replace(' ', '') for value in value.split(',')]
+                    name_filters = [func.replace(Gun.name, ' ', '').ilike(f"%{value.lower()}%") for value in clean_values]
+                    filter_list.extend(name_filters)
+                elif key == 'barrel_type':
+                    filter_list.append(Gun.barrel_type.in_(value))
+                elif key == 'caliber':
+                    caliber_values = [caliber.strip() for caliber in value.split(',')]
+                    caliber_filters = [Gun.caliber == caliber for caliber in caliber_values]
+                    filter_list.extend(caliber_filters)
+                elif key == 'category_id':
+                    category_ids_list = value.split(',')
+
+                    category_ids_int = []
+                    for category_id in category_ids_list:
+                        try:
+                            category_id_int = int(category_id)
+                            category_ids_int.append(category_id_int)
+                        except ValueError:
+                            raise HTTPException(status_code=400, detail=f"Invalid category ID: {category_id}")
+
+                    if not category_ids_int:
+                        raise HTTPException(status_code=400, detail="Category IDs are empty")
+
+                    category_filters = [Gun.category_id == category_id for category_id in category_ids_int]
+                    filter_list.extend(category_filters)
+
+            query = query.filter(or_(*filter_list))
+
+            result = await self.session.execute(query)
+            guns = result.scalars().all()
+        except NoResultFound:
+            raise HTTPException(status_code=404, detail="Guns not found")
+        return guns
--- api/routers/gun_router.py
+++ api/routers/gun_router.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.repositories.gun_repository import GunRepository
 from models.schemas import GunCreate, GunRead
 from utils.database import get_async_session
+from typing import Optional
 
 router = APIRouter(
     tags=["Gun"],
@@ -59,3 +60,27 @@ async def get_guns_by_name(name: str, session: AsyncSession = Depends(get_async_
 
     guns_by_name = await gun_repository.get_guns_by_name(name)
     return guns_by_name
+
+@router.get("/guns/search", response_model=List[GunRead])
+async def search_guns(
+        name: Optional[str] = None,
+        barrel_type: Optional[str] = None,
+        category_id: Optional[int] = None,
+        caliber: Optional[str] = None,
+        session: AsyncSession = Depends(get_async_session)
+):
+    gun_repository = GunRepository(session)
+
+    filters = {}
+    if name:
+        filters['name'] = name
+    if caliber:
+        filters['caliber'] = caliber
+    if barrel_type:
+        filters['barrel_type'] = barrel_type
+    if category_id:
+        filters['category_id'] = category_id
+
+    guns = await gun_repository.filter_guns(filters)
+    return guns
+
