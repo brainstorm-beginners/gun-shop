c12ec57c56029eceac602a52c62fcb3db48bfd59
Igor Ruzhilov <iruzhilov@gmail.com>
2023-10-23 19:36:48 +0600

    Finish header and its navigation-menu

--- frontend/main_page/mainPage.js
+++ frontend/main_page/mainPage.js
@@ -1,1 +1,53 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    // 'navigationMenu' elements' hover-pointer effect
+    let menuElements = document.querySelectorAll(".navigationElement");
+    for (let menuElement of menuElements) {
+      menuElement.addEventListener("mouseover", function() {
+        menuElement.style.cursor = "pointer";
+      });
+    };
 
+    let menuElement = document.getElementById("navigationElementExtendable");
+    menuElement.addEventListener("mouseover", function() {
+        menuElement.style.cursor = "pointer";
+    });
+
+    // 'searchIcon' element's hover-pointer effect
+    let searchIcon = document.getElementById("searchIcon");
+    searchIcon.addEventListener("mouseover", function() {
+        searchIcon.style.cursor = "pointer";
+    });
+
+    // 'extendableMenu' element's hover-appearing effect
+    let menuElementExtendable = document.getElementById("navigationElementExtendable");
+    let menuExtendable = document.getElementById("extendableMenu");
+    let isMenuVisible = false;
+
+    menuElementExtendable.addEventListener("mouseover", function() {
+        isMenuVisible = true;
+    });
+
+    menuExtendable.addEventListener("mouseout", function() {
+        isMenuVisible = false;
+    });
+
+    menuExtendable.addEventListener("mousemove", function() {
+        isMenuVisible = true;
+    });
+
+    setInterval(function() {
+    if (isMenuVisible) {
+        menuExtendable.style.display = "block";
+    } else {
+        menuExtendable.style.display = "none";
+    }
+    }, 1);
+
+    let extendableMenuElements = document.querySelectorAll(".extendableMenuElement");
+    for (let extendableMenuElement of extendableMenuElements) {
+      extendableMenuElement.addEventListener("mouseover", function() {
+        extendableMenuElement.style.cursor = "pointer";
+      });
+    };
+});
+  
